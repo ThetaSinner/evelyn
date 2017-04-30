af8fe77c00b13c83c6ba1ed37201013fe280c54a
Dylan Clarke <dylcla@me.com>
2017-04-30 22:38:21 +0100

    reduced error handling macro to single matcher

--- backend/src/core/error_messages.rs
+++ backend/src/core/error_messages.rs
@@ -29,18 +29,18 @@ pub enum EvelynServiceError {
     CouldNotDecodeTheRequestPayload(serde_json::Error),
     ForeignSessionToken(EvelynBaseError),
 
-    // user
+    // User
     CreateUser(EvelynCoreError),
     UserAlreadyExists(EvelynCoreError),
     LogonUser(EvelynCoreError),
     FailedToLogonUser(EvelynCoreError),
 
-    // simple task
+    // Simple Task
     FailedToCreateSimpleTask(EvelynCoreError),
     FailedToUpdateSimpleTask(EvelynCoreError),
     FailedToLookupSimpleTask(EvelynCoreError),
 
-    // todo list
+    // Todo List
     CreateTodoList(EvelynCoreError),
     AddItemToTodoList(EvelynCoreError),
     LookupTodoLists(EvelynCoreError),
@@ -48,58 +48,92 @@ pub enum EvelynServiceError {
     UpdateTodoListItem(EvelynCoreError),
 }
 
-macro_rules! EvelynServiceErrorDisplay {
-    ($({$x:ident::$x2:ident, $y:expr, $z:expr}),+) => {
-        impl fmt::Display for EvelynServiceError {
+macro_rules! EvelynErrorDisplay {
+    // Both error codes and error messages
+    ($x:ident, $({$x2:ident, $y:expr, $z:expr}),+) => (
+        impl fmt::Display for $x {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match *self {
-                    $($x::$x2(_) => write!(f, $y)),*
-                }
+                match *self { $($x::$x2(_) => write!(f, $y)),* }
             }
         }
 
-        impl error::Error for EvelynServiceError {
+        impl error::Error for $x {
             fn description(&self) -> &str {
-                match *self {
-                    $($x::$x2(_) => $z),*
-                }
+                match *self { $($x::$x2(_) => $z),* }
             }
 
             fn cause(&self) -> Option<&error::Error> {
-                match *self {
-                    $($x::$x2(ref e) => Some(e)),*
-                }
+                match *self { $($x::$x2(ref e) => Some(e)),* }
             }
         }
-    }
+    );
+
+    // Only error message, Error that refers to another error
+    ($x:ident, $({$x2:ident, $y:expr}),+) => (
+        impl fmt::Display for $x {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match *self { $($x::$x2(ref e) => write!(f, $y, e)),* }
+            }
+        }
+
+        impl error::Error for $x {
+            fn description(&self) -> &str {
+                match *self { $($x::$x2(_) => $y),* }
+            }
+
+            fn cause(&self) -> Option<&error::Error> {
+                match *self { $($x::$x2(ref e) => Some(e)),* }
+            }
+        }
+    );
+
+    // Only error message, no references to other errors
+    ($x:ident, $({$x2:ident}),+) => (
+        impl fmt::Display for $x {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match *self { $($x::$x2 => write!(f, "")),* }
+            }
+        }
+
+        impl error::Error for $x {
+            fn description(&self) -> &str {
+                match *self { $($x::$x2 => ""),* }
+            }
+
+            fn cause(&self) -> Option<&error::Error> {
+                match *self { $($x::$x2 => None),* }
+            }
+        }
+    );
 }
 
-EvelynServiceErrorDisplay!{
+EvelynErrorDisplay!{
+    EvelynServiceError,
     // Processing layer.
-    {EvelynServiceError::ReqestForActionWhichEvelynDoesNotKnowHowToDo, "100001", "Request for an action which Evelyn does now know how to do"},
-    {EvelynServiceError::EvelynTriedToHandleTheRequestButDidNotYieldAResponse, "100002", "Evelyn tried to handle the request but hasn't managed to give anything back"},
-    {EvelynServiceError::ExpectedHeaderOnRequestButNoneWasFound, "100003", "Expected a header with the request but didn't find a header"},
+    {ReqestForActionWhichEvelynDoesNotKnowHowToDo, "100001", "Request for an action which Evelyn does now know how to do"},
+    {EvelynTriedToHandleTheRequestButDidNotYieldAResponse, "100002", "Evelyn tried to handle the request but hasn't managed to give anything back"},
+    {ExpectedHeaderOnRequestButNoneWasFound, "100003", "Expected a header with the request but didn't find a header"},
 
-    {EvelynServiceError::CouldNotDecodeTheRequestPayload, "100101", "Could not decode the JSON request payload"},
-    {EvelynServiceError::ForeignSessionToken, "100102", "The server has been restarted please log on again"},
+    {CouldNotDecodeTheRequestPayload, "100101", "Could not decode the JSON request payload"},
+    {ForeignSessionToken, "100102", "The server has been restarted please log on again"},
 
     // User
-    {EvelynServiceError::CreateUser, "100201", "Failed to create user"},
-    {EvelynServiceError::UserAlreadyExists, "100202", "Failed to create user a user with that name already exists"},
-    {EvelynServiceError::LogonUser, "100203", "Invalid logon"},
-    {EvelynServiceError::FailedToLogonUser, "100204", "Failed to logon user"},
+    {CreateUser, "100201", "Failed to create user"},
+    {UserAlreadyExists, "100202", "Failed to create user a user with that name already exists"},
+    {LogonUser, "100203", "Invalid logon"},
+    {FailedToLogonUser, "100204", "Failed to logon user"},
 
     // Simple Task
-    {EvelynServiceError::FailedToCreateSimpleTask, "100301", "Failed to create simple task"},
-    {EvelynServiceError::FailedToUpdateSimpleTask, "100302", "Failed to update simple task"},
-    {EvelynServiceError::FailedToLookupSimpleTask, "100303", "Failed to lookup simple task"},
+    {FailedToCreateSimpleTask, "100301", "Failed to create simple task"},
+    {FailedToUpdateSimpleTask, "100302", "Failed to update simple task"},
+    {FailedToLookupSimpleTask, "100303", "Failed to lookup simple task"},
 
     // Todo List
-    {EvelynServiceError::CreateTodoList, "100401", "Failed to create todo list"},
-    {EvelynServiceError::AddItemToTodoList, "100402", "Failed to add item to todo list"},
-    {EvelynServiceError::LookupTodoLists, "100403", "Failed to lookup todo lists"},
-    {EvelynServiceError::LookupTodoList, "100404", "Failed to lookup todo list"},
-    {EvelynServiceError::UpdateTodoListItem, "100405", "Failed to update todo list item"}
+    {CreateTodoList, "100401", "Failed to create todo list"},
+    {AddItemToTodoList, "100402", "Failed to add item to todo list"},
+    {LookupTodoLists, "100403", "Failed to lookup todo lists"},
+    {LookupTodoList, "100404", "Failed to lookup todo list"},
+    {UpdateTodoListItem, "100405", "Failed to update todo list item"}
 }
 
 #[derive(Debug)]
@@ -124,51 +158,26 @@ pub enum EvelynCoreError {
     FailedToUpdateTodoListItem(EvelynDatabaseError),
 }
 
-macro_rules! EvelynCoreErrorDisplay {
-    ($({$x:ident::$x2:ident, $y:expr}),+) => {
-        impl fmt::Display for EvelynCoreError {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match *self {
-                    $($x::$x2(ref e) => write!(f, $y, e)),*
-                }
-            }
-        }
-
-        impl error::Error for EvelynCoreError {
-            fn description(&self) -> &str {
-                match *self {
-                    $($x::$x2(_) => $y),*
-                }
-            }
-
-            fn cause(&self) -> Option<&error::Error> {
-                match *self {
-                    $($x::$x2(ref e) => Some(e)),*
-                }
-            }
-        }
-    }
-}
-
-EvelynCoreErrorDisplay!{
+EvelynErrorDisplay!{
+    EvelynCoreError,
     //User
-    {EvelynCoreError::WillNotCreateUserBecauseUserAlreadyExists, "Will not create the requested user because that user already exists. {}"},
-    {EvelynCoreError::CannotCheckIfUserExistsSoWillNotCreateNewUser, "Cannot check if the user exists so a new user will not be ceated: {}"},
-    {EvelynCoreError::FailedToCreateUser, "Failed to create user: {}"},
-    {EvelynCoreError::InvalidLogon, "Invalid logon {}"},
-    {EvelynCoreError::FailedToLogonUser, "Failed to logon user: {}"},
+    {WillNotCreateUserBecauseUserAlreadyExists, "Will not create the requested user because that user already exists. {}"},
+    {CannotCheckIfUserExistsSoWillNotCreateNewUser, "Cannot check if the user exists so a new user will not be ceated: {}"},
+    {FailedToCreateUser, "Failed to create user: {}"},
+    {InvalidLogon, "Invalid logon {}"},
+    {FailedToLogonUser, "Failed to logon user: {}"},
 
     // Simple Task
-    {EvelynCoreError::FailedToCreateSimpleTask, "Failed to create task: {}"},
-    {EvelynCoreError::FailedToUpdateSimpleTask, "Failed to update task: {}"},
-    {EvelynCoreError::FailedToLookupSimpleTask, "Failed to lookup task: {}"},
+    {FailedToCreateSimpleTask, "Failed to create task: {}"},
+    {FailedToUpdateSimpleTask, "Failed to update task: {}"},
+    {FailedToLookupSimpleTask, "Failed to lookup task: {}"},
 
     //Todo List
-    {EvelynCoreError::FailedToCreateTodoList, "Failed to create todo list: {}"},
-    {EvelynCoreError::FailedToAddItemToTodoList, "Failed to add item to todo list: {}"},
-    {EvelynCoreError::FailedToLookupTodoLists, "Failed to lookup todo lists: {}"},
-    {EvelynCoreError::FailedToLookupTodoList, "Failed to lookup todo list: {}"},
-    {EvelynCoreError::FailedToUpdateTodoListItem, "Failed to update todo list item: {}"}
+    {FailedToCreateTodoList, "Failed to create todo list: {}"},
+    {FailedToAddItemToTodoList, "Failed to add item to todo list: {}"},
+    {FailedToLookupTodoLists, "Failed to lookup todo lists: {}"},
+    {FailedToLookupTodoList, "Failed to lookup todo list: {}"},
+    {FailedToUpdateTodoListItem, "Failed to update todo list item: {}"}
 }
 
 
@@ -194,51 +203,27 @@ pub enum EvelynDatabaseError {
     UpdateTodoListItem(MongoDbError),
 }
 
-macro_rules! EvelynDatabaseErrorDisplay {
-    ($({$x:ident::$x2:ident, $y:expr}),+) => {
-        impl fmt::Display for EvelynDatabaseError {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match *self {
-                    $($x::$x2(ref e) => write!(f, $y, e)),*
-                }
-            }
-        }
-
-        impl error::Error for EvelynDatabaseError {
-            fn description(&self) -> &str {
-                match *self {
-                    $($x::$x2(_) => $y),*
-                }
-            }
-
-            fn cause(&self) -> Option<&error::Error> {
-                match *self {
-                    $($x::$x2(ref e) => Some(e)),*
-                }
-            }
-        }
-    }
-}
-
-EvelynDatabaseErrorDisplay!{
-    {EvelynDatabaseError::SerialisationFailed, "Failed to serialise data for storage. {}"},
+EvelynErrorDisplay!{
+    EvelynDatabaseError,
+    // Processing
+    {SerialisationFailed, "Failed to serialise data for storage. {}"},
 
     // User
-    {EvelynDatabaseError::InsertUser, "Failed to create record for new user\n {}"},
-    {EvelynDatabaseError::LookupUser, "Failed to lookup user: {}"},
+    {InsertUser, "Failed to create record for new user\n {}"},
+    {LookupUser, "Failed to lookup user: {}"},
 
     // Simple Task
-    {EvelynDatabaseError::InsertSimpleTask, "Failed to create new simple task: {}"},
-    {EvelynDatabaseError::UpdateSimpleTask, "Failed to update simple task: {}"},
-    {EvelynDatabaseError::LookupSimpleTask, "Failed to lookup simple tasks: {}"},
+    {InsertSimpleTask, "Failed to create new simple task: {}"},
+    {UpdateSimpleTask, "Failed to update simple task: {}"},
+    {LookupSimpleTask, "Failed to lookup simple tasks: {}"},
 
     // Todo List
-    {EvelynDatabaseError::InsertTodoList, "Failed to insert todo list: {}"},
-    {EvelynDatabaseError::AddItemToTodoList, "Failed to add item to todo list: {}"},
-    {EvelynDatabaseError::LookupTodoLists, "Failed to lookup todo lists: {}"},
-    {EvelynDatabaseError::TodoListNotFound, "Todo list not found {}"},
-    {EvelynDatabaseError::LookupTodoList, "Failed to lookup todo list:  {}"},
-    {EvelynDatabaseError::UpdateTodoListItem, "Failed to update todo list item:  {}"}
+    {InsertTodoList, "Failed to insert todo list: {}"},
+    {AddItemToTodoList, "Failed to add item to todo list: {}"},
+    {LookupTodoLists, "Failed to lookup todo lists: {}"},
+    {TodoListNotFound, "Todo list not found {}"},
+    {LookupTodoList, "Failed to lookup todo list:  {}"},
+    {UpdateTodoListItem, "Failed to update todo list item:  {}"}
 }
 
 
@@ -249,30 +234,4 @@ pub enum EvelynBaseError {
     NothingElse
 }
 
-macro_rules! EvelynBaseErrorDisplay {
-    ($({$x:ident::$x2:ident, $y:expr}),+) => {
-        impl fmt::Display for EvelynBaseError {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match *self {
-                    $($x::$x2 => write!(f, $y)),*
-                }
-            }
-        }
-
-        impl error::Error for EvelynBaseError {
-            fn description(&self) -> &str {
-                match *self {
-                    $($x::$x2 => $y),*
-                }
-            }
-
-            fn cause(&self) -> Option<&error::Error> {
-                match *self {
-                    $($x::$x2 => None),*
-                }
-            }
-        }
-    }
-}
-
-EvelynBaseErrorDisplay!({EvelynBaseError::NothingElse, ""});
+EvelynErrorDisplay!(EvelynBaseError, {NothingElse});
