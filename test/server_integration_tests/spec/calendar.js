d99d51915ed3b1812ad66e63a22602110ef891c3
Dylan Clarke <dylcla@me.com>
2017-05-06 12:36:12 +0100

    add initial calendar integration test

---
+++ test/server_integration_tests/spec/calendar.js
@@ -0,0 +1,38 @@
+if (!global.Promise) {
+  global.Promise = require('bluebird');
+}
+
+var expect = require('chai').expect;
+
+var httpHelper = require('../helpers/chai-http-request-helper.js');
+
+describe('Calendar', function() {
+  var date = new Date ().toISOString();
+  var token = null;
+
+  before(function () {
+    return httpHelper.chaiHttpPostPurgeDatabase()
+      .then(function () {
+        return httpHelper.createUserAndLogon();
+      })
+      .then(function (_token) {
+          token = _token;
+      });
+  });
+
+  it('Add Event', function() {
+    return httpHelper.chaiHttpPost(
+      '/calendar/addevent',
+      {
+        Token: token,
+        Title: "The Great Testing Event",
+        EventBegin: date,
+        EventEnd: date
+      }
+    )
+    .then(function (response) {
+      expect(response.Error).to.be.null;
+    });
+  });
+
+});
