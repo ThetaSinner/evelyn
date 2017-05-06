b73c0dcd49cc470f6125c34d31c35a469c0f7016
Dylan Clarke <dylcla@me.com>
2017-05-06 12:37:22 +0100

    simple task integration test - update simple task

--- test/server_integration_tests/spec/simple_task.js
+++ test/server_integration_tests/spec/simple_task.js
@@ -12,12 +12,12 @@ describe('Simple Task', function() {
 
   before(function () {
     return httpHelper.chaiHttpPostPurgeDatabase()
-      .then(function () {
-        return httpHelper.createUserAndLogon();
-      })
-      .then(function (_token) {
-          token = _token;
-      });
+    .then(function () {
+      return httpHelper.createUserAndLogon();
+    })
+    .then(function (_token) {
+      token = _token;
+    });
   });
 
   it('Create', function() {
@@ -38,7 +38,7 @@ describe('Simple Task', function() {
   // TODO asserts for the tests below aren't checking enough.
 
   describe("Lookup", function () {
-    it('Lookup - No Limit, No Completed', function() {
+    it('Fetches unlimited unfinished tasks', function() {
       return httpHelper.chaiHttpPost(
         '/simpletask/lookup',
         {
@@ -52,7 +52,7 @@ describe('Simple Task', function() {
       });
     });
 
-    it('Lookup - Limit, No Completed', function() {
+    it('Fetches 10 unfinished tasks', function() {
       return httpHelper.chaiHttpPost(
         '/simpletask/lookup',
         {
@@ -66,7 +66,7 @@ describe('Simple Task', function() {
       });
     });
 
-    it('Lookup - No Limit, Inc Completed', function() {
+    it('Fetches unlimited tasks, including completed', function() {
       return httpHelper.chaiHttpPost(
         '/simpletask/lookup',
         {
@@ -80,7 +80,7 @@ describe('Simple Task', function() {
       });
     });
 
-    it('Lookup - Limit, Inc Completed', function() {
+    it('Fetches 10 tasks, including completed', function() {
       return httpHelper.chaiHttpPost(
         '/simpletask/lookup',
         {
@@ -92,6 +92,119 @@ describe('Simple Task', function() {
       .then(function (response) {
         expect(response.Error).to.be.null;
       });
+    });
+  });
+
+  describe("Update", function () {
+    var simpletask;
+
+    before(function () {
+      return httpHelper.chaiHttpPost(
+        '/simpletask/create',
+        {
+          Token: token,
+          Title: "Test Task",
+          Description: "Descriptive",
+          DueDate: date
+        }
+      )
+      .then(function (response) {
+        expect(response.Error).to.be.null;
+        return httpHelper.chaiHttpPost(
+          '/simpletask/lookup',
+          {
+            Token: token,
+            ShowCompleted: false,
+            Limit: 10
+          }
+        )
+      })
+      .then(function (response) {
+        expect(response.Error).to.be.null;
+        expect(response.SimpleTasks).to.not.be.null;
+        simpletask = response.SimpleTasks[0];
+
+        expect(simpletask.taskId).to.not.be.null;
+        expect(simpletask.title).to.not.be.null;
+        expect(simpletask.description).to.not.be.null;
+        expect(simpletask.dueDate).to.not.be.null;
+        expect(simpletask.completed).to.not.be.null;
+      });
+    });
+
+    it('Changes title', function() {
+      return httpHelper.chaiHttpPost(
+        '/simpletask/update',
+        {
+          Token: token,
+          TaskId: simpletask.taskId,
+          NewTitle: "This is a new title",
+          NewDescription: simpletask.fescription,
+          NewDueDate: simpletask.dueDate,
+          NewCompleted: true,
+        }
+      ).then(
+        function (response) {
+          expect(response.Error).to.be.null;
+        }
+      );
+    });
+
+    it('Changes description', function() {
+      return httpHelper.chaiHttpPost(
+        '/simpletask/update',
+        {
+          Token: token,
+          TaskId: simpletask.taskId,
+          NewTitle: simpletask.title,
+          NewDescription: "Describe this ...",
+          NewDueDate: simpletask.dueDate,
+          NewCompleted: simpletask.completed,
+        }
+      ).then(
+        function (response) {
+          expect(response.Error).to.be.null;
+        }
+      );
+    });
+
+    it('Changes duedate', function() {
+      var newDate = new Date();
+      newDate.setDate(newDate.getDate() + 1);
+
+      return httpHelper.chaiHttpPost(
+        '/simpletask/update',
+        {
+          Token: token,
+          TaskId: simpletask.taskId,
+          NewTitle: simpletask.title,
+          NewDescription: simpletask.fescription,
+          NewDueDate: newDate.toISOString(),
+          NewCompleted: simpletask.completed,
+        }
+      ).then(
+        function (response) {
+          expect(response.Error).to.be.null;
+        }
+      );
+    });
+
+    it('Mark as complete', function() {
+      return httpHelper.chaiHttpPost(
+        '/simpletask/update',
+        {
+          Token: token,
+          TaskId: simpletask.taskId,
+          NewTitle: simpletask.title,
+          NewDescription: simpletask.fescription,
+          NewDueDate: simpletask.dueDate,
+          NewCompleted: true,
+        }
+      ).then(
+        function (response) {
+          expect(response.Error).to.be.null;
+        }
+      );
     });
   });
 });
