5aacd06bc7f81b44916d8a0bfbb94fcf1cc6b0d4
ThetaSinner <greatdjonfire@hotmail.co.uk>
2017-06-15 00:17:02 +0100

    Test - create tests for search for user

--- test/server_integration_tests/spec/user.js
+++ test/server_integration_tests/spec/user.js
@@ -135,4 +135,90 @@ describe('User', function() {
             });
         });
     });
+
+    describe('Search for users', function() {
+        beforeEach(function () {
+            return httpHelper.chaiHttpPostPurgeDatabaseArea('user');
+        });
+
+        it('Searches for a user', function() {
+            return httpHelper.createUserAndLogon('user1')
+            .then(function(token) {
+                return httpHelper.searchForUsers(token, 'er');
+            })
+            .then(function(response) {
+                expect(response.SearchResults).to.be.an.array;
+                expect(response.SearchResults).to.have.lengthOf(1);
+
+                var result = response.SearchResults[0];
+
+                expect(result.UserName).to.equal('user1');
+                expect(result.UserId).to.be.ok;
+            });
+        });
+
+        it('Searches for a matching user when there are multiple users', function() {
+            var token = null;
+
+            return httpHelper.createUserAndLogon('user1')
+            .then(function(_token) {
+                token = _token;
+                return httpHelper.createUserAndLogon('jimmy');
+            })
+            .then(function() {
+                return httpHelper.searchForUsers(token, 'er');
+            })
+            .then(function(response) {
+                expect(response.SearchResults).to.be.an.array;
+                expect(response.SearchResults).to.have.lengthOf(1);
+
+                var result = response.SearchResults[0];
+
+                expect(result.UserName).to.equal('user1');
+                expect(result.UserId).to.be.ok;
+            })
+            .then(function() {
+                return httpHelper.searchForUsers(token, 'mm');
+            })
+            .then(function(response) {
+                expect(response.SearchResults).to.be.an.array;
+                expect(response.SearchResults).to.have.lengthOf(1);
+
+                var result = response.SearchResults[0];
+
+                expect(result.UserName).to.equal('jimmy');
+                expect(result.UserId).to.be.ok;
+            });
+        });
+
+        it('Searches and finds multiple users', function() {
+            var token = null;
+
+            return httpHelper.createUserAndLogon('user1')
+            .then(function(_token) {
+                token = _token;
+                return httpHelper.createUserAndLogon('user2');
+            })
+            .then(function() {
+                return httpHelper.createUserAndLogon('jimmy');
+            })
+            .then(function() {
+                return httpHelper.searchForUsers(token, 'er');
+            })
+            .then(function(response) {
+                expect(response.SearchResults).to.be.an.array;
+                expect(response.SearchResults).to.have.lengthOf(2);
+
+                var result1 = response.SearchResults[0];
+
+                expect(result1.UserName).to.equal('user1');
+                expect(result1.UserId).to.be.ok;
+
+                var result2 = response.SearchResults[1];
+
+                expect(result2.UserName).to.equal('user2');
+                expect(result2.UserId).to.be.ok;
+            });
+        });
+    });
 });
